a1a320da6b52b48500adf1919fd4ca2129b879a9
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-08-31 20:18:36 +0300

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,13 @@
+export const Data = [
+    {
+        question:'what do you call a dog magician',
+        answer:'A labrador'
+    },
+    {
+        question:'what do you call a dog magician',
+        answer:'A labrador'
+    },    {
+        question:'what do you call a dog magician',
+        answer:'A labrador'
+    }
+];
